3e51c51868933e591beca3342b153edf94b4019a
Piotr Laszczkowski <swistach@gmail.com>
2021-04-07 23:42:59 +0200

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-mat-hot';
+  instance1 = {
+    startRows: 50,
+    colHeaders: true,
+    stretchH: 'all',
+  };
+  instance2 = {
+    startRows: 50,
+    colHeaders: true,
+    stretchH: 'all',
+    manualColumnResize: true,
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { HotTableModule } from '@handsontable/angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    HotTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
